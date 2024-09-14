ae45bf5f4cc14d2c56084da64858738d6337badc
SpatialNebula <55620776+SpatialNebula@users.noreply.github.com>
2024-09-14 02:28:12 +0200

    Fix bug making entire note become the mention

--- modules/commands/checkingroup.js
+++ modules/commands/checkingroup.js
@@ -52,6 +52,7 @@ module.exports = {
         const guildMembers = mapMembers(EN).concat(mapMembers(PC)).concat(mapMembers(SC)).concat(mapMembers(E)).concat(mapMembers(SE)).concat(mapMembers(M))
 
         let notInGroup = [];
+        let userIdsNotInGroup = [];
         let inGroup = [];
 
         interaction.reply({ephemeral: false, content: `Checking 0/${guildMembers.length} discord users. May take approximately ${Math.ceil((0.5*guildMembers.length)/60)} minutes total.`})
@@ -83,7 +84,7 @@ module.exports = {
 
             let current = `[${i}]\tR${rowifi_data.roblox_id}\tD${guildMembers[i]}\tG`
 
-            if (filteredGroups.length < 1) {current += "false"; notInGroup.push(current)} else {current += "true", inGroup.push(rowifi_data.roblox_id)};
+            if (filteredGroups.length < 1) {current += "false"; userIdsNotInGroup.push(guildMembers[i]); notInGroup.push(current)} else {current += "true", inGroup.push(rowifi_data.roblox_id)};
             console.log(current)
 
             interaction.editReply({ephemeral: false, content: `Checking ${(parseInt(i)+1)}/${guildMembers.length} discord users. May take approximately ${Math.ceil((0.5*guildMembers.length)/60)} minutes total.\n\nFlagged not in group: <@${notInGroup.join(">, <@")}>`})
@@ -93,6 +94,6 @@ module.exports = {
         let robloxMembers = await getRobloxMembers();
         interaction.editReply({ephemeral: false, content: `Filtering roblox members...\n\nFlagged not in group: <@${notInGroup.join("> <@")}>`})
         let notInDiscord = robloxMembers.filter(member => !inGroup.includes(member.userId)).map(member => member.username)
-        interaction.editReply({ephemeral: false, content: `Fetching all roblox members...\n\nFlagged not in group: <@${notInGroup.join("> <@")}>\n\nFlagged not in discord: \`${notInDiscord.join("`, `")}\``})
+        interaction.editReply({ephemeral: false, content: `Fetching all roblox members...\n\nFlagged not in group: <@${notInGroup.join("> <@")}>\n\nFlagged not in discord: \`${userIdsNotInGroup.join("`, `")}\``})
     }
 }
