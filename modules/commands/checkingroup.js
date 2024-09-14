0c8024552f566e682f7a4528aab49dcf8f9ab101
SpatialNebula <55620776+SpatialNebula@users.noreply.github.com>
2024-09-14 02:30:35 +0200

    Edit previous mistake

--- modules/commands/checkingroup.js
+++ modules/commands/checkingroup.js
@@ -87,13 +87,13 @@ module.exports = {
             if (filteredGroups.length < 1) {current += "false"; userIdsNotInGroup.push(guildMembers[i]); notInGroup.push(current)} else {current += "true", inGroup.push(rowifi_data.roblox_id)};
             console.log(current)
 
-            interaction.editReply({ephemeral: false, content: `Checking ${(parseInt(i)+1)}/${guildMembers.length} discord users. May take approximately ${Math.ceil((0.5*guildMembers.length)/60)} minutes total.\n\nFlagged not in group: <@${notInGroup.join(">, <@")}>`})
+            interaction.editReply({ephemeral: false, content: `Checking ${(parseInt(i)+1)}/${guildMembers.length} discord users. May take approximately ${Math.ceil((0.5*guildMembers.length)/60)} minutes total.\n\nFlagged not in group: <@${userIdsNotInGroup.join(">, <@")}>`})
         }
 
         interaction.editReply({ephemeral: false, content: `Fetching all roblox members...\n\nFlagged not in group: <@${notInGroup.join("> <@")}>`})
         let robloxMembers = await getRobloxMembers();
         interaction.editReply({ephemeral: false, content: `Filtering roblox members...\n\nFlagged not in group: <@${notInGroup.join("> <@")}>`})
         let notInDiscord = robloxMembers.filter(member => !inGroup.includes(member.userId)).map(member => member.username)
-        interaction.editReply({ephemeral: false, content: `Fetching all roblox members...\n\nFlagged not in group: <@${notInGroup.join("> <@")}>\n\nFlagged not in discord: \`${userIdsNotInGroup.join("`, `")}\``})
+        interaction.editReply({ephemeral: false, content: `Fetching all roblox members...\n\nFlagged not in group: <@${notInGroup.join("> <@")}>\n\nFlagged not in discord: \`${notInDiscord.join("`, `")}\``})
     }
 }
